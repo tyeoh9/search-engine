22df08eb7aa3cb6c9d277a22a1d8fa50b633b003
chckn9 <thomasyeoh9@gmail.com>
2026-01-12 11:10:40 -0800

    Add Document class boilerplate

---
+++ src/main/java/search/ingest/Document.java
@@ -0,0 +1,14 @@
+package search.ingest
+
+public final class Document {
+
+    private final int id;
+    private final String title;
+    private final String text;
+
+    public Document(int id, String title, String text) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+    }
+}
