495f22e1dc5301094e0400eb2dde5cc923390d3e
chckn9 <thomasyeoh9@gmail.com>
2026-01-13 15:10:56 -0800

    Add example usage

--- src/main/java/search/ingest/WikiJsonReader.java
+++ src/main/java/search/ingest/WikiJsonReader.java
@@ -17,6 +17,18 @@ public class WikiJsonReader {
 
     /**
      * Reads a JSON file line by line and returns an iterator of Document objects
+     *
+     * Usage:
+     * WikiJsonReader reader = new WikiJsonReader();
+     *
+     * try (InputStream in = Files.newInputStream(Path.of("wiki_500.json"))) {
+     *     MappingIterator<Document> docs = reader.readDocuments(in);
+     *
+     *     while (docs.hasNext()) {
+     *         Document doc = docs.next();
+     *         // index document
+     *     }
+     * }
      */
     public MappingIterator<Document> readDocuments(InputStream in) throws IOException {
         return MAPPER.readerFor(Document.class).readValues(in);
