04e38e36c3af555dc0415b06f5fadc576ef19c64
chckn9 <thomasyeoh9@gmail.com>
2026-01-13 15:19:21 -0800

    Implement document ingestion logic

--- src/main/java/search/ingest/IngestRunner.java
+++ src/main/java/search/ingest/IngestRunner.java
@@ -2,3 +2,27 @@
  * Reads input files (datasets), parses it, and passes it to the indexer
  */
 
+package search.ingest;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class IngestRunner {
+
+    public static void main(Path file) throws Exception {
+        WikiJsonReader read = new WikiJsonReader();
+
+        try (InputStream in = Files.newInputStream(file) {
+            MappingIterator<Document> docs = reader.readDocuments(in);
+
+            while (docs.hasNext()) {
+                Document doc = docs.next();
+                // TODO: Process document
+                // TODO: Index document
+            }
+        }
+    }
+}
