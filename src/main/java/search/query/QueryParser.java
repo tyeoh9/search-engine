b6a12dd7e9dd3d2dbb30b4216b5cf0e4bcb161e9
chckn9 <thomasyeoh9@gmail.com>
2026-01-17 13:22:38 -0800

    Implement basic QueryParser class

--- src/main/java/search/query/QueryParser.java
+++ src/main/java/search/query/QueryParser.java
@@ -1,3 +1,17 @@
 /**
  * Parses a user query string into normalized tokens suitable for searching the inverted index
  */
+
+package search.query;
+
+import search.analysis.Tokenizer;
+
+import java.util.List;
+
+public class QueryParser {
+
+    // Parse user query, normalize, and tokenize
+    public List<String> parse(String rawQuery) {
+        return Tokenizer.tokenize(rawQuery);
+    }
+}
