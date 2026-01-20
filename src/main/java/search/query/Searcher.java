6221d734a9549e3675192e8cf31a94e1b7243aa0
chckn9 <thomasyeoh9@gmail.com>
2026-01-19 22:42:47 -0800

    Outline method for searching related documents

--- src/main/java/search/query/Searcher.java
+++ src/main/java/search/query/Searcher.java
@@ -1,3 +1,13 @@
 /**
  * Executes queries against the inverted index and ranks documents based on token matches and frequencies
+ *
+ * Method:
+ * 1. Accumulate scores for each document given the tokenized query
+ * 2. Use a hashmap to keep track of docScores (e.g. docId -> score);
+ *    'score' here would be sum of occurrences of all terms or weighted sum
+ * 3. Sort documents based on score
+ * 4. Return top k matching documents
+ * 5. Try using TF-IDF instead
+ *
  */
+
