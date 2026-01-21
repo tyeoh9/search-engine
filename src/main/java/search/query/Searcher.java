905d08ff538f88a590f21abe338249f8019fd026
chckn9 <thomasyeoh9@gmail.com>
2026-01-20 21:07:58 -0800

    Implement logic for scoring documents (using term freq)

--- src/main/java/search/query/Searcher.java
+++ src/main/java/search/query/Searcher.java
@@ -13,20 +13,37 @@
 
 package search.query;
 
+import search.index.InvertedIndex;
+import search.index.Posting;
+
 import java.util.HashMap;
+import java.util.List;
 
 public class Searcher {
 
     // Maps docId -> score
-    private HashMap<Integer, Double> docScores;
+    private final HashMap<Integer, Double> docScores;
+    private final InvertedIndex index;
 
-    public Searcher() {
+    public Searcher(InvertedIndex index) {
         this.docScores = new HashMap<>();
+        this.index = index;
     }
 
     // Increase score for docId by additionalScore
     public void increaseScore(int docId, double additionalScore) {
         this.docScores.merge(docId, additionalScore, Double::sum);
+    }
+
+    // Score documents against query using term frequencies (tf)
+    public void scoreDocuments(List<String> tokenizedQuery) {
+        for (String token : tokenizedQuery) {
+            List<Posting> postings = this.index.getPostings(token);
+
+            for (Posting p : postings) {
+                this.increaseScore(p.getDocId(), p.getFrequency());
+            }
+        }
     }
 
 }
