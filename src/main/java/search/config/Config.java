704477ecd45c83386495040684856679d3d11208
chckn9 <thomasyeoh9@gmail.com>
2026-01-21 11:51:23 -0800

    Edit config

--- src/main/java/search/config/Config.java
+++ src/main/java/search/config/Config.java
@@ -11,10 +11,14 @@ public final class Config {
     // Prevent instantiation
     private Config() {}
 
+    // Dataset size
+    public static final int SIZE = 100000;
+
     // Data paths
-    public static final Path WIKI_DATASET = Path.of("data/wiki_10000.json");
-    public static final Path INDEX_PATH = Path.of("data/index_10000.json");
-    public static final Path DOC_TITLES_PATH = Path.of("data/doc_titles_10000.json");
+    public static final Path WIKI_DATASET = Path.of(String.format("data/wiki_%d.json", SIZE));
+    public static final Path INDEX_PATH = Path.of(String.format("data/index_%d.json", SIZE));
+    public static final Path DOC_TITLES_PATH = Path.of(String.format("data/doc_titles_%d.json", SIZE));
+
 
     // Query settings
     public static final int TOP_K_RESULTS = 5;
